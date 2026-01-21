9212d6383724ddc4320b2e3e9a60950792211bb0
amolmane1939 <amolmane1939@gmail.com>
2026-01-21 23:26:07 +0530

    adding buildspec files

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,6 +8,15 @@ provider "aws" {
     }
   }
 }
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-600627315506-ap-south-1 "
+    key            = "environments/{env}/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
 
 module "networking" {
   source = "../../modules/networking"
--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -9,6 +9,17 @@ provider "aws" {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-600627315506-ap-south-1 "
+    key            = "environments/{env}/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+
 module "networking" {
   source = "../../modules/networking"
   region = var.region
